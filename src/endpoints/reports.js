d6cb9d2ea860f334e66f3c8635d457fdd21464f7
assemaybm <assem4real@yahoo.com>
2021-08-28 21:17:50 +0200

    updated the Check model

---
+++ src/endpoints/reports.js
@@ -0,0 +1,4 @@
+
+
+// TODO: create the report gen endpoints here
+// TODO: put the main function in the utils file 
--- src/models/Check.js
+++ src/models/Check.js
@@ -1,33 +1,65 @@
 const { DataTypes } = require("sequelize");
-const database = require("../config/dbConfig")
+const database = require("../config/dbConfig");
 
+const Check = database.define("Check", {
+  id: {
+    type: DataTypes.INTEGER(),
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING(),
+    allowNull: false,
+  },
 
-const Check = database.define(
-  "Check",
-  {
-    id: {
-      type: DataTypes.INTEGER(),
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    name: {
-      type: DataTypes.STRING(),
-      allowNull: false,
-    },
+  url: {
+    type: DataTypes.STRING(),
+    allowNull: false,
+  },
+  protocol: {
+    type: DataTypes.STRING(),
+    allowNull: false,
+  },
+  path: {
+    type: DataTypes.STRING(),
+    allowNull: true,
+  },
 
-    url: {
-        type: DataTypes.STRING(),
-        allowNull: false,
-    },
-    protocol: {
-        type: DataTypes.STRING(),
-        allowNull: false,
-    },
-  
+  // webhook url
+  webhook: {
+    type: DataTypes.STRING(),
+    allowNull: false,
+  },
 
-    tableName: "checks",
-  }
-);
+  timeout: {
+    type: DataTypes.INTEGER(),
+    allowNull: false,
+    defaultValue: 5000 /** in ms */,
+  },
+
+  interval: {
+    type: DataTypes.BIGINT(),
+    allowNull: false,
+    defaultValue: 1000 * 60 * 10 /** 10 mins */,
+  },
+
+  username: {
+    type: DataTypes.STRING(),
+    allowNull: false,
+  },
+
+  password: {
+    type: DataTypes.STRING(),
+    allowNull: false,
+  },
+
+  tag: {
+    type: DataTypes.TEXT(),
+    allowNull: false,
+  },
+
+  tableName: "checks",
+});
 
 module.exports = {
   Check,
